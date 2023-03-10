c93a153b1a21f514aa2981105562072af764f409
Piyush Baijal <56352367+piyushbaijal@users.noreply.github.com>
2023-03-10 12:13:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         }
 
     });
-    // // smooth scrolling 
+    // // smooth scrolling
     $('a[href*="#"]').on('click', function (e) {
         // e.preventDefault();
         $('html, body').animate({
@@ -23,13 +23,28 @@ $(document).ready(function () {
         }, 1000,);
     });
 
-    // // active link or hower link 
+    // active link or hower link
     const links = document.querySelectorAll("header .navbar ul li a");
-    links.forEach(link => link.addEventListener("click", e => {
+    links.forEach(link => link.addEventListener("click", () => {
         links.forEach((link) => link.classList.remove("home"));
         link.classList.add("home");
     }));
-
     links[0].classList.add("home");
 
+
+    const navLinks = document.querySelectorAll('nav a');
+    const sections = document.querySelectorAll('section');
+
+    window.addEventListener('scroll', () => {
+        const headerHeight = document.querySelector('header').offsetHeight;
+        const currentSection = [...sections]
+            .find(sec => window.scrollY >= (sec.offsetTop - headerHeight) && window.scrollY <= (sec.offsetTop + sec.offsetHeight - headerHeight));
+        const currentLink = document.querySelector(`nav a[href="#${currentSection.id}"]`);
+        navLinks.forEach(link => link.classList.remove('home'));
+        currentLink.classList.add('home');
+    });
+
 });
+
+
+
